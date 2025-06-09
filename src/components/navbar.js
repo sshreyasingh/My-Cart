adee4fa18bdccdcfe9ee624111566d0a941207c3
sshreyasingh <161379376+sshreyasingh@users.noreply.github.com>
2025-06-10 01:17:08 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,10 +6,21 @@ class Navbar extends React.Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
-      <h2>My Cart</h2>
+      // <h2>My Cart</h2>
+        <h2 style={{ 
+  color: "white", 
+  textAlign: 'center', 
+  fontSize: '2rem', 
+  fontWeight: 'bold', 
+  marginBottom: '20px',
+  fontStyle: 'italic',
+  fontFamily: `Oswald`  
+}}>
+  My Cart Aisle
+</h2>    
     </a>
     <a className="navbar-brand" href="#">
-    <h1 className="display-3">Welcome to My Cart</h1>
+    <h1 className="display-3">Welcome to My Cart Aisle</h1>
     </a>
   </div>
 </nav>
